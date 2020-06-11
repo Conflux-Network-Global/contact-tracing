b40084fae8ce3e8310b16325c4f110ab45e72144
aalu1418 <aalu1418@gmail.com>
2020-06-11 12:03:16 -0400

    basic test cases complete

--- test/contactTracing_test.js
+++ test/contactTracing_test.js
@@ -15,6 +15,7 @@ contract("Contact tracing flow", async (accounts) => {
   let ind1Hash = [];
   const user0 = { from: accounts[0] };
   const user1 = { from: accounts[1] };
+  const gasPrice = new BN(web3.utils.toWei("20", "gwei"));
 
   before(async () => {
     reg = await Registration.deployed();
@@ -39,39 +40,90 @@ contract("Contact tracing flow", async (accounts) => {
     expect(ind1.address).to.not.equal(reg.address);
   });
 
-  // it("registerPayload provides reward on first payload registration", async () => {
-  //   const balance_prev = await web3.eth.getBalance(user0.from);
-  //   const output = await reg.registerPayload(ind0Payload[0], user0);
-  //   const balance_after = await web3.eth.getBalance(user0.from);
-  //   ind0Hash.push(output.logs[0].args.payload);
-  //
-  //   console.log(
-  //     new BN(balance_after).add(
-  //       web3.utils.toWei(new BN(output.receipt.gasUsed)),
-  //       "gwei"
-  //     ),
-  //     new BN(balance_prev)
-  //   );
-  //   const check = new BN(balance_after).add(
-  //     web3.utils.toWei(new BN(output.receipt.gasUsed), "gwei")
-  //   );
-  //   expect(check.gt(new BN(balance_prev))).to.be.true;
-  // });
-  //
-  // it("registerPayload does not provide reward on second payload registration", async () => {
-  //   const balance_prev = await web3.eth.getBalance(user0.from);
-  //   const output = await reg.registerPayload(ind0Payload[1], user0);
-  //   const balance_after = await web3.eth.getBalance(user0.from);
-  //   ind0Hash.push(output.logs[0].args.payload);
-  //
-  //   const check = new BN(balance_after).add(
-  //     web3.utils.toWei(new BN(output.receipt.gasUsed), "gwei")
-  //   );
-  //   expect(check.eq(new BN(balance_prev))).to.be.true;
-  //
-  //   //register other payloads
-  //   const output0 = await reg.registerPayload(ind1Payload[0], user1);
-  //   const output1 = await reg.registerPayload(ind1Payload[1], user1);
-  //   ind1Hash = [output0, output1];
-  // });
+  it("registerPayload provides reward on first payload registration", async () => {
+    const balance_prev = await web3.eth.getBalance(user0.from);
+    const output = await reg.registerPayload(ind0Payload[0], user0);
+    const balance_after = await web3.eth.getBalance(user0.from);
+    ind0Hash.push(output.logs[0].args.payload);
+
+    const check = new BN(balance_after).add(
+      gasPrice.mul(new BN(output.receipt.gasUsed))
+    );
+    expect(check.gt(new BN(balance_prev))).to.be.true;
+  });
+
+  it("registerPayload does not provide reward on second payload registration", async () => {
+    const balance_prev = await web3.eth.getBalance(user0.from);
+    const output = await reg.registerPayload(ind0Payload[1], user0);
+    const balance_after = await web3.eth.getBalance(user0.from);
+    ind0Hash.push(output.logs[0].args.payload);
+
+    const check = new BN(balance_after).add(
+      gasPrice.mul(new BN(output.receipt.gasUsed))
+    );
+    expect(check.eq(new BN(balance_prev))).to.be.true;
+
+    //register other payloads
+    const output0 = await reg.registerPayload(ind1Payload[0], user1);
+    const output1 = await reg.registerPayload(ind1Payload[1], user1);
+    ind1Hash = [output0.logs[0].args.payload, output1.logs[0].args.payload];
+  });
+
+  it("registerPayload reverts if payload is already registered", async () => {
+    await truffleAssert.reverts(reg.registerPayload(ind1Payload[0], user1));
+    await truffleAssert.reverts(reg.registerPayload(ind1Payload[0], user0));
+  });
+
+  it("registerPayload reverts if user is not registerd", async () => {
+    await truffleAssert.reverts(
+      reg.registerPayload("0x5", { from: accounts[5] })
+    );
+  });
+
+  it("contact function returns correct contact address", async () => {
+    const output0 = await ind0.contact(ind1Payload[0], user0);
+    const output1 = await ind1.contact(ind0Payload[0], user1);
+
+    assert(
+      output0.logs[0].args.contact == ind1Hash[0],
+      "returned address does not match registered address"
+    );
+    assert(
+      output1.logs[0].args.contact == ind0Hash[0],
+      "returned address does not match registered address"
+    );
+  });
+
+  it("checkStatus reverts when not called by registration contract", async () => {
+    await truffleAssert.reverts(ind0.checkStatus(ind0Hash[0]));
+  });
+
+  it("toggleHealth works without reverting when called correctly", async () => {
+      await truffleAssert.passes(ind1.toggleHealth(user1));
+  })
+
+  it("toggleHealth reverts when not called by owner", async () => {
+    await truffleAssert.reverts(ind0.toggleHealth(user1));
+  })
+
+  it("checkHealth returns health status for individual when matching payloads are submitted", async () => {
+
+    const health1 = await reg.checkHealth.call(ind0Hash[0], ind1Hash[0], user0);
+    const health0 = await reg.checkHealth.call(ind1Hash[0], ind0Hash[0], user1);
+
+    expect(health1).to.not.be.true;
+    expect(health0).to.be.true;
+  });
+
+  it("checkHealth reverts with incorrect inputs", async () => {
+    await truffleAssert.reverts(
+      reg.checkHealth.call(ind0Hash[0], reg.address, user0)
+    ); //invalid contact address
+    await truffleAssert.reverts(
+      reg.checkHealth.call(ind1Hash[0], ind1Hash[0], user0)
+    ); //invalid personal payload address
+    await truffleAssert.reverts(
+      reg.checkHealth.call(ind0Hash[1], ind1Hash[0], user0)
+    ); //contact did not recieve users token (does not return health)
+  });
 });
