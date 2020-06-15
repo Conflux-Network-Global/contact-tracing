85d57534d9c82dce44c409e51bfa705ff7bf1a17
aalu1418 <aalu1418@gmail.com>
2020-06-15 15:17:33 -0400

    Updates

---
+++ precache-manifest.b44da6d721b5c01ef10c0d6549de93da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1fa960dcdf641c33f88d62c75801e7",
+    "url": "/contact-tracing/index.html"
+  },
+  {
+    "revision": "7cbd470ced317acbcb61",
+    "url": "/contact-tracing/static/css/main.064e25a2.chunk.css"
+  },
+  {
+    "revision": "cfda2aafd1fbd4d745a2",
+    "url": "/contact-tracing/static/js/2.b9e0f80b.chunk.js"
+  },
+  {
+    "revision": "71f08bb8ad75542702a8ef6c3e6386cd",
+    "url": "/contact-tracing/static/js/2.b9e0f80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cbd470ced317acbcb61",
+    "url": "/contact-tracing/static/js/main.8d8c7c40.chunk.js"
+  },
+  {
+    "revision": "3ce587da00f652dea034",
+    "url": "/contact-tracing/static/js/runtime-main.77ce8369.js"
+  }
+]);
